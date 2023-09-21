0eef831b369ca37d5c0d8a7e667122c50b08c0d2
rawr <103551740+samontejirahmarie@users.noreply.github.com>
2023-09-21 16:00:32 +0800

    blur

--- script/script.js
+++ script/script.js
@@ -13,12 +13,31 @@ function openSideMenu() {
 }
 // window.addEventListener('scroll', function () {
 //     var verticalScroll = window.scrollY;
-//     if (verticalScroll > 2910){
-//         document.querySelector('.bg-container').classList.remove('sticky');
+//     if (verticalScroll >= 1280){
+//         document.querySelector('.hero-greeting').classList.add('blur');
 //     }
 //     else{
-//         this.document.querySelector('.bg-container').classList.add('sticky')
+//         this.document.querySelector('.hero-greeting').classList.remove('blur')
 //     }
 //     // console.log('Vertical Scroll Position: ' + verticalScroll + 'px');
 //     // console.log('Horizontal Scroll Position: ' + horizontalScroll + 'px');
 // });
+const blurElement = document.querySelector('.hero-greeting');
+const startBlur = 1280; 
+
+window.addEventListener('scroll', () => {
+     const scrollY = window.scrollY;
+
+    if (scrollY >= startBlur) {
+        const maxBlur = 12; 
+        const blurAmount = Math.min(maxBlur, (scrollY - startBlur) / 10); 
+
+       
+        blurElement.style.filter = `blur(${blurAmount}px)`;
+    }
+    else{
+        blurAmount = 0;
+        blurElement.style.filter = `blur(${blurAmount}px)`;
+    }
+});
+
